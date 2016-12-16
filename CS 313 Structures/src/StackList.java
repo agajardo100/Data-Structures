ea22fd39fa55cc3182c2264ddbbaeebb28b3c258
agajardo100 <andresegajardo@gmail.com>
2016-12-15 21:19:52 -0500

    Iterator and toString

--- CS 313 Structures/src/StackList.java
+++ CS 313 Structures/src/StackList.java
@@ -1,8 +1,10 @@
+import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class StackList<T extends Comparable<T>> {
+public class StackList<AnyType> implements Iterable<AnyType> {
 	
-	Node<T> top;		//Top of stack
+	private Node<AnyType> top;		//Top of stack
+	private int size;				//Number of elements in Stack
 	
 	protected class Node<E> {
 		public E data;
@@ -13,31 +15,99 @@ public class StackList<T extends Comparable<T>> {
 			next = n;
 		}
 	}
-		
-		public boolean isEmpty(){
-			return top == null;
-		}
-		
-		public void push(T item){
-			top = new Node<T>(item, top);
-		}
-		
-		public T pop(){
-			T pop = top.data;
-			top = top.next;
-			return pop;
-		}
-		
-		public void makeEmpty(){
-			if (isEmpty())
-				throw new NoSuchElementException("Stack is already empty");
-			top = null;
-		}
-		
-		public T peek(){
-			if (isEmpty())
-				throw new NoSuchElementException("Stack is empty");
-			return top.data;
-		}
-		
+	
+	/* Constructor */
+	public StackList(){
+		top = null;
+		size = 0;
 	}
+	
+	/* Returns the number of elements in Stack */
+	public int size(){
+		return size;
+	}
+	
+	/* Returns True if top is null, otherwise
+	 * return false.
+	 */
+	public boolean isEmpty(){
+		return top == null;
+	}
+	
+	/*
+	 * Makes Stack logically empty setting top Node equal to null
+	 */
+	public void makeEmpty(){
+		if (isEmpty())
+			throw new NoSuchElementException("Stack is already empty");
+		top = null;
+		size = 0;
+	}
+	
+	/* Append new element to Stack and set it as the top
+	 * Best/Avg/Worst Runtime: O(1)
+	 */
+	public void push(AnyType item){
+		top = new Node<AnyType>(item, top);
+		size++;
+	}
+	
+	/* Remove top element off the Stack and return it 
+	 * Best/Avg/Worst Runtime: O(1)
+	 */
+	public AnyType pop(){
+		if (isEmpty())
+			throw new NoSuchElementException("Stack is empty");
+		AnyType oldTop = top.data;
+		top = top.next;
+		size--;
+		return oldTop;
+	}
+	
+	/* Return the top element 
+	 * Best/Avg/Worst Runtime: O(1)
+	 */
+	public AnyType peek(){
+		if (isEmpty())
+			throw new NoSuchElementException("Stack is empty");
+		return top.data;
+	}
+	
+	/* Return String representation of Stack */
+	@Override
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+	    for (AnyType element : this)
+	    	s.append(element.toString() + " ");
+	    return s.toString();
+	    }
+	
+	/* 
+	 * Returns an Interator that goes through the Stack, 
+	 * starting at the top 
+	 */
+	@Override
+	public Iterator<AnyType> iterator() {
+		return new StackIterator();
+	}
+	
+	private class StackIterator implements Iterator<AnyType>{
+		private Node<AnyType> current = top;
+		
+		@Override
+		public boolean hasNext(){ 
+        	return current != null;
+        }
+		
+		@Override
+        public AnyType next() {
+            if (!hasNext()) 
+            	throw new NoSuchElementException();
+            AnyType data = current.data;
+            current = current.next; 
+            return data;
+		}
+	}
+	
+	
+}
